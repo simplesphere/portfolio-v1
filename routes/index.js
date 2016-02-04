71444ac5e887aeeacd50cbfed1d0ba858b61962b
simplesphere <anoop.jeewoolall@gmail.com>
2016-02-03 23:08:30 -0500

    Added dummy files / contents + routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home Page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About Page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET Skills Page. */
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Service' });
+});
+
+/* GET Portfolio Page. */
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'Project' });
+});
+
+/* GET Contact Page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
